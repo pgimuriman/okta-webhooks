1a66cbfa13207609242f9ec756b383eb30b89a4c
Tom Smith <tom.smith@okta.com>
2019-05-12 12:14:36 -0400

    add destination url

--- app.js
+++ app.js
@@ -35,18 +35,36 @@ app.post('/event_hooks', function (req, res) {
 
 	console.dir(JSON.stringify(req.body))
 
-	res.json(req.body)
+	route_request(req.body)
+
+	// res.json(req.body)
 
 })
 
 app.get('/event_hooks', function (req, res) {
 	console.log("received a verification request from okta")
 
-	console.log("the header is: " + req.header("X-Okta-Verification-Challenge"))
+	const verification_header = "X-Okta-Verification-Challenge"
+
+	console.log("the header is: " + req.header(verification_header))
 
 	var response_obj = {}
-	response_obj["verification"] = req.header("X-Okta-Verification-Challenge")
+
+	response_obj["verification"] = req.header(verification_header)
 
 	res.json(response_obj)
 
 })
+
+function route_request(hook_obj) {
+
+	// const url = "https://webhook.site/8a1c25d1-2a62-4864-8a02-bf5c74d72dc4"
+
+	request.post(HOOK_DEST_01, {form:{msg:'got an event hook from Okta!'}})
+
+	return
+
+	// request.post('http://service.com/upload', {form:{key:'value'}})
+
+
+}
